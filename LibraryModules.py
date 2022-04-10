e1d4b486c4a250f55daaf50bebdd3dd1b7ede919
Linda <lindaallinson123@gmail.com>
2022-04-10 14:27:00 +0100

    book_id matching user_input issue solved - it wasn't a matching issue in the end, it was the for loop clashing with the if statements. Also the update worked by adding conn.commit and taking the result variable out too.

--- LibraryModules.py
+++ LibraryModules.py
@@ -27,23 +27,26 @@ def get_user_search_results():
 
 
 def check_if_book_available_to_borrow():
-    book_number = input(f"Would you like to borrow a book? If so enter the book_id or press q to quit: ")
+    match_found = 0
+    book_number = int(input("Would you like to borrow a book? If so enter the book_id or press q to quit: "))
     engine = create_engine("mysql+pymysql://root:password@localhost/library", echo=False, future=True)
     with engine.connect() as conn:
-        result = conn.execute(text(f"SELECT book_id, book_title, on_loan, reserved FROM books"))
+        result = conn.execute(text("""SELECT book_id, book_title, on_loan, reserved FROM books"""))
         for row in result:
-            if book_number in str(row.book_id):
-                request_to_borrow_book(book_number)
+            if book_number == row.book_id and row.on_loan == "no" and row.reserved == "no":
+                match_found = 1
                 break
-            elif book_number == str(row.book_id) and row.on_loan == "yes":
-                print(f"Sorry, {row.book_title} is on loan already, maybe you could reserve it.")
+            elif book_number == row.book_id and row.on_loan == "yes" and row.reserved == "no":
+                print("Sorry, this book is on loan but you can reserve it")
                 break
-            elif book_number == str(row.book_id) and row.on_loan == "no" and row.reserved == "yes":
-                print(f"Sorry, {row.book_title} is reserved for somebody else and will be held for them for 7 days.")
+            elif book_number == row.book_id and row.on_loan == "no" and row.reserved == "yes":
+                print("Sorry, this book is available but is reserved for someone, please enquire again in 7 days")
                 break
-            elif book_number != str(row.book_id):
-                print(f"Book ID {book_number} does not exist")
-                break
+            else:
+                pass
+    if match_found == 1:
+        request_to_borrow_book(book_number)
+
 
 def request_to_borrow_book(book_id):
     engine = create_engine("mysql+pymysql://root:password@localhost/library", echo=False, future=True)
@@ -53,30 +56,35 @@ def request_to_borrow_book(book_id):
             ON books.branch_id = branch.branch_id""")
         )
         for row in result:
-            if book_id == str(row.book_id):
+            if book_id == row.book_id:
                 print(f"The book {row.book_title} by {row.author} is located at the {row.branch_address} branch.")
                 if row.on_loan == "no" and row.reserved =="no":
-                    library_card = input('Would you like to borrow it? press y for yes or press q to quit')
-                    if library_card == "y":
+                    user_input = input('Would you like to borrow it? press y for yes or press q to quit')
+                    if user_input == "y":
                         make_book_on_loan_in_db(book_id)
                         print('All done. Happy Reading')
                     else:
                         print('Thank you for using the library, goodbye')
                 else: print(f"Book is either on loan already or reserved, check back in one week")
 
-def make_book_on_loan_in_db(book_number):
+
+def make_book_on_loan_in_db(book_id):
     engine = create_engine("mysql+pymysql://root:password@localhost/library", echo=False, future=True)
     with engine.connect() as conn:
         result = conn.execute(
             text("""SELECT book_id, book_title, author, branch_number, on_loan, reserved, branch_address FROM books INNER JOIN branch
-            ON books.branch_id = branch.branch_id"""))
+            ON books.branch_id = branch.branch_id""")
+        )
         for row in result:
-            if book_number == row.book_id:
-                text(f"UPDATE books SET on_loan = 'yes' WHERE book_id = {row.book_id}")
-                conn.commit()
-                print("Mischief managed")
-                print(row.book_id, row.on_loan)
+            if book_id == row.book_id:
+                with engine.connect() as conn:
+                    conn.execute (text(f"UPDATE books SET on_loan = 'yes' WHERE book_id = {row.book_id}")
+                    )
+                    conn.commit()
+                    print("Mischief Managed")
 
-# once got the matching ids working add the additional updates in make_book_on_loan_in_db (loan table, user history etc).
-# add reserve function to work the same way
+
+
+# need to add additional updates in make_book_on_loan_in_db (eg loan table, user history etc).
+# then add reserve function to work the same way
 
